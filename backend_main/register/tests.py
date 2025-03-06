352243997af0bddb243a0c3eb6afe4225bd84d90
elena-17 <95474265+elena-17@users.noreply.github.com>
2025-03-06 09:52:07 +0100

    test(back): add request tests

--- backend_main/register/tests.py
+++ backend_main/register/tests.py
@@ -1,10 +1,16 @@
+import requests
+import requests_mock
+
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 from register.models import User
 
 
 class UserTestCase(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.user = User.objects.create(
             email="test@example.com",
             phone_number="+1234567890",
@@ -13,9 +19,44 @@ class UserTestCase(TestCase):
             credit_card_number="1234567890123456",
         )
 
-    def test_user_creation(self):
+    def test_user_creation(self) -> None:
         self.assertEqual(self.user.email, "test@example.com")
         self.assertEqual(self.user.phone_number, "+1234567890")
         self.assertEqual(self.user.name, "Test User")
         self.assertEqual(self.user.id_number, "1234567890")
         self.assertEqual(self.user.credit_card_number, "1234567890123456")
+
+
+class RegisterUserTests(APITestCase):
+    def setUp(self) -> None:
+        self.url = reverse("register_user")
+
+    @requests_mock.Mocker()
+    def test_register_user_valid_card(self, mock_request) -> None:
+        mock_request.post("http://yours/creditscards/validate", status_code=200)
+
+        data = {
+            "credit_card": "1234567890123456",
+        }
+        response = self.client.post(self.url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    @requests_mock.Mocker()
+    def test_register_user_invalid_card(self, mock_request) -> None:
+        mock_request.post("http://yours/creditscards/validate", status_code=400)
+
+        data = {
+            "credit_card": "1234567890123456",
+        }
+        response = self.client.post(self.url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @requests_mock.Mocker()
+    def test_register_user_error_validating_card(self, mock_request) -> None:
+        mock_request.post("http://yours/creditscards/validate", exc=requests.exceptions.RequestException)
+
+        data = {
+            "credit_card": "1234567890123456",
+        }
+        response = self.client.post(self.url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
