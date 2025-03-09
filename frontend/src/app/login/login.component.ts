95c25620e13d5f8169a02aaf781219f0e99c6788
elena-17 <95474265+elena-17@users.noreply.github.com>
2025-03-09 20:15:25 +0100

    feat: implement login functionality with form validation and styling
    ------------
    issue #10

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,111 @@
+import { Component } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+
+@Component({
+  selector: "app-login",
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    CommonModule,
+  ],
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  email_id: string = "";
+  password: string = "";
+  hidePassword = true;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email_id: ["", [Validators.required, emailIdValidator()]],
+      password: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          createPasswordStrengthValidator(),
+        ],
+      ],
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log("Form Submitted", this.loginForm.value);
+    } else {
+      console.log("Form is invalid");
+    }
+  }
+
+  getErrorMessage(controlName: string): string {
+    const control = this.loginForm.get(controlName);
+    // if (control?.hasError('required')) {
+    //   return `${controlName.charAt(0).toUpperCase() + controlName.slice(1)} is required`;
+    // }
+    if (control?.hasError("email")) {
+      return "Invalid email or ID number";
+    }
+    if (control?.hasError("minlength")) {
+      return "Password must be at least 8 characters long";
+    }
+    if (control?.hasError("passwordStrength")) {
+      return "Password must include uppercase, lowercase, and numbers";
+    }
+    return "";
+  }
+}
+
+///////////////////////////////////////////////////////////////////
+export function createPasswordStrengthValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+    if (!value) {
+      return null;
+    }
+    const hasUpperCase = /[A-Z]+/.test(value);
+    const hasLowerCase = /[a-z]+/.test(value);
+    const hasNumeric = /[0-9]+/.test(value);
+    const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+    return !passwordValid ? { passwordStrength: true } : null;
+  };
+}
+
+export function emailIdValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+    if (!value) {
+      return null;
+    }
+    const emailIdValid =
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+    const idNumberValid = /^[0-9]{8}[a-zA-Z]$/.test(value);
+
+    if (!emailIdValid && !idNumberValid) {
+      return { email: true };
+    }
+    return null;
+  };
+}
