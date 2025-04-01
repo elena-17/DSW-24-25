5aa8c13506e39485da61b427b3d9f7f01218fd64
elena-17 <95474265+elena-17@users.noreply.github.com>
2025-04-01 21:16:26 +0200

    fix: get transaction error

--- backend_main/runserver.bat
+++ backend_main/runserver.bat
@@ -2,7 +2,7 @@
 docker compose up -d db_main
 
 echo Esperando a que la base de datos este lista...
-timeout /t 5 /nobreak >nul
+timeout /t 2 /nobreak >nul
 
 python manage.py migrate
 python manage.py runserver 8000
