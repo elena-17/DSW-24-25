d5097f52580f0615d392792117d29ea889048966
Antonio Campallo Gómez <ancago4@gmail.com>
2025-04-02 11:37:18 +0200

    feat(back): getting accounts from users and tested it

--- backend_main/backend_main/urls.py
+++ backend_main/backend_main/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("users.urls")),
     path("creditcards/", include("creditcard.urls")),
+    path("account/", include("account.urls")),
 ]
