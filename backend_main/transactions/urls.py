1a4b6964d49303f8b20e317ba81bfda9ab174951
elena-17 <95474265+elena-17@users.noreply.github.com>
2025-04-02 05:18:48 +0200

    fix: incorrect url for get sender and receiver

--- backend_main/transactions/urls.py
+++ backend_main/transactions/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from .views import (
     get_all_transactions,
+    get_all_transactions_receiver,
+    get_all_transactions_sender,
     get_transaction,
     request_money,
     send_money,
@@ -12,8 +14,8 @@ from .views import (
 # url pattern = /transaction/....
 urlpatterns = [
     path("", get_all_transactions, name="get_transactions"),
-    path("sender/", get_all_transactions, name="get_transactions_sender"),
-    path("receiver/", get_all_transactions, name="get_transactions_receiver"),
+    path("sender/", get_all_transactions_sender, name="get_transactions_sender"),
+    path("receiver/", get_all_transactions_receiver, name="get_transactions_receiver"),
     path("send-money/", send_money, name="send_money"),
     path("request-money/", request_money, name="request_money"),
     path("<int:id>/", get_transaction, name="get_transaction"),
