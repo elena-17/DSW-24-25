344c89d3c2af5cd7435cbc027f004ccefb2ef532
Antonio Campallo Gómez <ancago4@gmail.com>
2025-04-11 11:26:24 +0200

    feat(back): Admin accounts logic added

--- backend_main/account/urls.py
+++ backend_main/account/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import adding_money, get_account, subtracting_money
-
+from .views.viewsUser import adding_money, get_account, subtracting_money
+from .views.viewsAdmin import get_all_accounts, update_account_balance
 app_name = "account"
 
 urlpatterns = [
     path("", get_account, name="get-account"),
     path("recharge/", adding_money, name="adding-money"),
     path("withdraw/", subtracting_money, name="subtracting-money"),
+    path("accounts/", get_all_accounts, name="get-accounts"),
+    path("update/", update_account_balance, name="update-account-balance")
 ]
