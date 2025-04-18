c7b2db02fde80d15617eeab18b506a6bf9989467
Antonio Campallo Gómez <ancago4@gmail.com>
2025-04-18 12:04:36 +0200

    feat(back): Adding request to stripe and response

--- backend_main/account/urls.py
+++ backend_main/account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views.viewsUser import adding_money, get_account, subtracting_money
+from .views.viewsUser import adding_money, get_account, subtracting_money, paymentRequest
 from .views.viewsAdmin import get_all_accounts, update_account_balance
 app_name = "account"
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", get_account, name="get-account"),
     path("recharge/", adding_money, name="adding-money"),
     path("withdraw/", subtracting_money, name="subtracting-money"),
+    path("payment-request/", paymentRequest, name="payment-request"),
     path("accounts/", get_all_accounts, name="get-accounts"),
     path("update/", update_account_balance, name="update-account-balance")
 ]
