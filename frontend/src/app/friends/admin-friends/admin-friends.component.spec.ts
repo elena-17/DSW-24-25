b01439a15d8b9d6078a7b38f7b2a4fe410ac604c
Antonio Campallo Gómez <ancago4@gmail.com>
2025-04-22 19:30:36 +0200

    feat(front): Adding component for admin friends

---
+++ frontend/src/app/friends/admin-friends/admin-friends.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminFriendsComponent } from './admin-friends.component';
+
+describe('AdminFriendsComponent', () => {
+  let component: AdminFriendsComponent;
+  let fixture: ComponentFixture<AdminFriendsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdminFriendsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdminFriendsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/friends/admin-friends/admin-friends.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-friends',
+  imports: [],
+  templateUrl: './admin-friends.component.html',
+  styleUrl: './admin-friends.component.scss'
+})
+export class AdminFriendsComponent {
+
+}
