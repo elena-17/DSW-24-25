b5096a5450ab79fc0170f3c59fb01d03c1969834
elena-17 <95474265+elena-17@users.noreply.github.com>
2025-05-07 19:34:56 +0200

    chore: add database dump scripts

---
+++ dumps_and_scripts/import_db_bank.sh
@@ -0,0 +1,23 @@
+CONTAINER_NAME="zap-postgres-bank"
+DB_NAME="zap_db_bank"
+DB_USER="zap_admin"
+FILE="dump_bank_20250506_225051.sql"
+FILE_PATH="/tmp/$FILE"
+DECOMPRESSED_FILE="/tmp/dump.sql"
+
+echo "Checking container '$CONTAINER_NAME'..."
+if ! docker ps | grep -q "$CONTAINER_NAME"; then
+    echo "The container '$CONTAINER_NAME' is not running. Aborting."
+    exit 1
+fi
+
+echo "Copying dump file to container..."
+docker cp "$FILE" "$CONTAINER_NAME:$FILE_PATH"
+
+echo "Decompressing dump file inside the container..."
+docker exec -i "$CONTAINER_NAME" gunzip -f "$FILE_PATH.gz"
+
+echo "Restoring database '$DB_NAME'..."
+docker exec -i "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -f "$FILE_PATH"
+
+echo "Database restoration completed."
