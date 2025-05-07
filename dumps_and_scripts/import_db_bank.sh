80cd0d6ee09df18fee9c622353e241b753936035
Antonio Campallo Gómez <ancago4@gmail.com>
2025-05-08 00:29:10 +0200

    FIX import_db_bank.sh

--- dumps_and_scripts/import_db_bank.sh
+++ dumps_and_scripts/import_db_bank.sh
@@ -1,9 +1,8 @@
 CONTAINER_NAME="zap-postgres-bank"
 DB_NAME="zap_db_bank"
 DB_USER="zap_admin"
-FILE="dump_bank_20250506_225051.sql"
+FILE="dump_bank_20250506_225051.sql.gz"
 FILE_PATH="/tmp/$FILE"
-DECOMPRESSED_FILE="/tmp/dump"
 
 echo "Checking container '$CONTAINER_NAME'..."
 if ! docker ps | grep -q "$CONTAINER_NAME"; then
@@ -15,9 +14,11 @@ echo "Copying dump file to container..."
 docker cp "$FILE" "$CONTAINER_NAME:$FILE_PATH"
 
 echo "Decompressing dump file inside the container..."
-docker exec -i "$CONTAINER_NAME" gunzip -f "$FILE_PATH.gz"
+docker exec -i "$CONTAINER_NAME" gunzip -f "$FILE_PATH"
+
+FILE_PATH_SQL="${FILE_PATH%.gz}"
 
 echo "Restoring database '$DB_NAME'..."
-docker exec -i "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -f "$FILE_PATH"
+docker exec -i "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -f "$FILE_PATH_SQL"
 
 echo "Database restoration completed."
